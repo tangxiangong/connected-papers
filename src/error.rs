8595e4be62b785d553598ed0a6ac20f00da306a9
tangxiangong <tangxiangong@gmail.com>
2025-12-24 14:52:58 +0800

    feat: Add custom error handling for API interactions with Error enum and Result type

---
+++ src/error.rs
@@ -0,0 +1,17 @@
+#[derive(Debug, thiserror::Error, Clone, PartialEq)]
+pub enum Error {
+    #[error(transparent)]
+    APIKeyNotFound(#[from] std::env::VarError),
+    #[error(transparent)]
+    SemanticScholarRequestFailed(#[from] crate::ss::client::RequestFailedError),
+    #[error("{0}")]
+    ReqwestError(String),
+}
+
+impl From<reqwest::Error> for Error {
+    fn from(error: reqwest::Error) -> Self {
+        Error::ReqwestError(error.to_string())
+    }
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
